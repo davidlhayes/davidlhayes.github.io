487d2db72c94d3319253bb9a8f0d3741adb4b42a
David Hayes <davidlynnhayes@att.net>
2015-07-13 18:15:27 -0500

    first pass on contact form

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -2,4 +2,32 @@ $(document).ready(function() {
 
   console.log("All resources are loaded");
 
+// contact submit event handler
+  $('#form_send').on('click',function() {
+    var formdata = app.createFormObject();
+    console.log('Clicked form submit...');
+  });
+
+    // $.ajax
+
+
 });
+
+var app = app || {};
+app.createFormObject = function() {
+
+  var retJson = {};
+
+  retJson.first_name = $('#first_name').val();
+  retJson.last_name = $('#last_name').val();
+  retJson.user_email = $('#user_email').val();
+  retJson.user_phone = $('#user_phone').val();
+  retJson.movie = $('#movie').val();
+  retJson.adventure = $('#heist').val();
+
+  // test...comment out when done;
+  console.log(retJson);
+
+  return retJson;
+
+}
