6597e000722acbc249b8c0788bcb6a251902894f
David Hayes <davidlynnhayes@att.net>
2015-07-14 16:19:05 -0500

    Tuesday afternoon

--- project1/README.md
+++ project1/README.md
@@ -1,2 +1,6 @@
-# jQueryBoilerplate
-A simple jQuery Boilerplate for students to use!
+# Choose Your adventure
+## A Project by David Hayes
+## General Assembly Web Development Immersive
+### Chicago Cohort #2 - July 17, 2015
+
+This is a demostration website promoting an imaginary business.
--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
 
   console.log("All resources are loaded");
 
-// contact submit event handler
-  $('#form_send').on('click',function() {
+  // hide status messages
+  $('.loading').hide();
+  $('.success').hide();
+  $('.error').hide();
+
+  // contact submit event handler
+  $('#form_send').on('click', function() {
     var formdata = app.createFormObject();
+    console.log(formdata);
     console.log('Clicked form submit...');
+    app.sendEmail(formdata);
+
   });
-
-    // $.ajax
-
 
 });
 
@@ -18,16 +23,49 @@ app.createFormObject = function() {
 
   var retJson = {};
 
-  retJson.first_name = $('#first_name').val();
-  retJson.last_name = $('#last_name').val();
-  retJson.user_email = $('#user_email').val();
-  retJson.user_phone = $('#user_phone').val();
-  retJson.movie = $('#movie').val();
-  retJson.adventure = $('#heist').val();
+  retJson.userName = $('#user_name').val();
+  retJson.userEmail = $('#user_email').val();
+  retJson.request = $('#user_request').val();
 
-  // test...comment out when done;
-  console.log(retJson);
+  // testing... comment out when done
+  //console.log(retJson);
 
   return retJson;
 
 }
+app.sendEmail = function(emailData) {
+
+  // display loading info...
+  $('.loading').slideDown(); // .show()
+  // hide old messages... because this is a NEW request
+  $('.success').hide();
+  $('.error').hide();
+
+  // ajax argument
+  var ajaxData = {
+    url: 'http://imperialholonet.herokuapp.com/api/mail',
+    type: 'POST',
+    data: emailData,
+    success: function(data) {
+      console.log(data);
+      $('.loading').slideUp(); // .hide()
+      $('.success').show(); // .slideDown()
+    },
+    error: function(err) {
+      console.log(err);
+      $('.loading').slideUp(); // .hide()
+      $('.error').show();
+    }
+
+  };
+
+  $.ajax(ajaxData);
+
+}
+
+  // retJson.first_name = $('#first_name').val();
+  // retJson.last_name = $('#last_name').val();
+  // retJson.user_email = $('#user_email').val();
+  // retJson.user_phone = $('#user_phone').val();
+  // retJson.movie = $('#movie').val();
+  // retJson.adventure = $('#heist').val();
