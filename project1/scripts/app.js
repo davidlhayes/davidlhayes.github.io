90d900c69e40acb6c2bfcd4be9b1bcf1ce358dea
David Hayes <davidlynnhayes@att.net>
2015-07-16 23:24:23 -0500

    finished media query

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -24,31 +24,56 @@
     console.log('Clicked form submit...');
     app.sendEmail(formdata);
 
+
+
   });
 
-
+// buttons
   $('#inquiries').click(function() {
     $('#contact_form_wrapper').hide().slideDown('slow');
   });
-
-
   $('#contact_dismiss').click(function() {
     $('#contact_form_wrapper').show().slideUp('slow');
   });
-
   $('#pricing_select').click(function() {
     $('#pricing').hide().slideDown('slow');
   });
-
   $('#pricing_dismiss').click(function() {
     $('#pricing').show().slideUp('slow');
   });
+  // legal buttons
+  $('#legal-inquiries').click(function() {
+    $('#contact_form_wrapper').hide().slideDown('slow');
+  });
+  $('#legal-contact_dismiss').click(function() {
+    $('#contact_form_wrapper').show().slideUp('slow');
+  });
+  $('#legal-pricing_select').click(function() {
+    $('#pricing').hide().slideDown('slow');
+  });
+  $('#legal-pricing_dismiss').click(function() {
+    $('#pricing').show().slideUp('slow');
+  });
+  // faq buttons
+  $('#faq-inquiries').click(function() {
+    $('#contact_form_wrapper').hide().slideDown('slow');
+  });
+  $('#contact_dismiss').click(function() {
+    $('#contact_form_wrapper').show().slideUp('slow');
+  });
+  $('#faq-pricing-select').click(function() {
+    $('#pricing').hide().slideDown('slow');
+  });
+  $('#faq-pricing_dismiss').click(function() {
+    $('#pricing').show().slideUp('slow');
+  });
+
 
 // Adventure data
 
 var pirate = { adv_img : "pirate-action.png", adv_text : "Want to live in a world that resembles some popular Johnny Depp movies? Enticed by the idea of obscene treasure? There’s plenty of adventure aboard our pirate ship. You may be a captain or you may lead the mutiny, but somebody is definitely walking the plank!"};
 var heist = { adv_img : "heist-action.png", adv_text : "Banks these days tend to be so, so secure. It’s a good thing you’ll have the technology, the tools, and the support team to plan a robbery of staggering proportions. We can’t promise you’ll succeed or walk home with millions of dollars to stash in a mattress, but the adrenaline rush will be amazing!" };
-var spy = { adv_img : "spy-action.png", adv_text : "We’ll spill the beans right now. This experience starts with a kidnapping: yours. Rest assured, one way or another, you’ll escape, but in the process you’ll learn of larger threat and you’ll be the only one who can stop." };
+var spy = { adv_img : "spy-action.png", adv_text : "We’ll spill the beans right now. This experience starts with a kidnapping: yours. Rest assured, one way or another, you’ll escape, but in the process you’ll learn of larger threat and you’ll be the only one who can stop it." };
 var phonebooth = { adv_img : "time-action.png", adv_text : "Professor Copernicus Von Braun has converted a London call box into a working time machine. The trouble is that it’s hard to say exactly where you’ll end up. Rest assured, you’ll have the “time” of your life AND a return trip to the present is guaranteed" };
 
 var htmlstring = "";
@@ -87,6 +112,39 @@ $('.four').click(function() {
   console.log(htmlstring);
   $('#descr-right').html(htmlstring);
   $('#descr-right').fadeTo(2000,1);
+});
+
+// Phone functions
+$('#ph1').click(function() {
+  htmlstring = '<img src="assets/images/' + phonebooth.adv_img
+    + '"><p>"' + phonebooth.adv_text + '"</p>';
+  console.log(htmlstring);
+  $('#phone_descr').html(htmlstring);
+  $('#phone_descr').fadeTo(2000,1);
+});
+
+$('#ph2').click(function() {
+  htmlstring = '<img src="assets/images/' + spy.adv_img
+    + '"><p>"' + spy.adv_text + '"</p>';
+  console.log(htmlstring);
+  $('#phone_descr').html(htmlstring);
+  $('#phone_descr').fadeTo(2000,1);
+});
+
+$('#ph3').click(function() {
+  htmlstring = '<img src="assets/images/' + pirate.adv_img
+    + '"><p>"' + pirate.adv_text + '"</p>';
+  console.log(htmlstring);
+  $('#phone_descr').html(htmlstring);
+  $('#phone_descr').fadeTo(2000,1);
+});
+
+$('#ph4').click(function() {
+  htmlstring = '<img src="assets/images/' + heist.adv_img
+    + '"><p>"' + heist.adv_text + '"</p>';
+  console.log(htmlstring);
+  $('#phone_descr').html(htmlstring);
+  $('#phone_descr').fadeTo(2000,1);
 });
 
 });
